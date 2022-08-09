e13b0a45a4d21217b53422296e1799c45235e614
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-08-09 18:25:23 +0900

    I screwed up the Django project name. I accidentally made it 'src', so this commit changes it to 'mai_shogi'

--- web/server/init_django_project.sh
+++ web/server/init_django_project.sh
@@ -1,2 +1,2 @@
 # use the Django instance that was installed to the docker image to scaffold the project
-sudo docker-compose run web django-admin startproject src .
+sudo docker-compose run web django-admin startproject mai_shogi .
