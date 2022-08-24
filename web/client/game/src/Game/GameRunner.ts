aa8e1d7d27463141a8a7863e0b03ca569fd5ea48
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-08-24 22:06:35 +0900

    trying to load the PNG for the board spaces. I loaded the image properly but the texture is not being properly applied to the board
    also did a bunch of work for calculating the locations of the spaces on
    the board

--- web/client/game/src/Game/GameRunner.ts
+++ web/client/game/src/Game/GameRunner.ts
@@ -1,9 +1,10 @@
-import { BufferGeometry, Camera, Color, DoubleSide, Group, Material, Mesh, MeshBasicMaterial, Object3D, OrthographicCamera, Scene, ShapeGeometry, Vector3, WebGLRenderer } from "three";
+import { BufferGeometry, Camera, Color, DoubleSide, Group, Material, Mesh, MeshBasicMaterial, Object3D, OrthographicCamera, PlaneGeometry, Scene, ShapeGeometry, Texture, Vector3, WebGLRenderer } from "three";
 import { SVGLoader, SVGResult } from "three/examples/jsm/loaders/SVGLoader.js";
 import { makeExistsGuard } from "../utils/Guards";
+import { buildForRange } from "../utils/Range";
 
 import { createGame } from "./GameCreator";
-import { RenderSettings } from "./Renderer/Renderer";
+import { defaultRenderSettings, RenderSettings } from "./Renderer/Renderer";
 import { getAssetKeyForPiece } from "./types/AssetKeys";
 import { Game } from "./types/Game";
 import { HeldPiece, isHeldPiece, isPlaced, Piece } from "./types/Piece";
@@ -53,6 +54,9 @@ export class GameRunner {
 	private checkDefined = makeExistsGuard("GameRunner");
 
 	private renderSettings?: RenderSettings;
+	private renderSettingsOrDefault(): RenderSettings {
+		return this.renderSettings !== undefined ? this.renderSettings : defaultRenderSettings();
+	}
 
 	private _canvas?: HTMLCanvasElement;
 	public setCanvas(newCanvas: HTMLCanvasElement) {
@@ -86,6 +90,8 @@ export class GameRunner {
 		return this.checkDefined(this._scene, "getScene() scene undefined");
 	}
 
+	private images: {[index: string]: any;} = {};
+
 	/**
 	* not sure if it's okay to just store the materials and geometries and then make the
 	* meshes as they are needed, or if the meshes should also be cached
@@ -105,7 +111,60 @@ export class GameRunner {
 		this.setScene(new Scene());
 		this.initRenderer();
 
-		const svgRequestResults: [string, SVGResult][] = await Promise.all(this.prepareSvgs(boardAndPiecesSvgSetting));
+		const textureRequestInfo: [string, string][] = [
+			[
+				"tile_texture",
+				this.formImagePath([
+					boardAndPiecesSvgSetting.rootDir,
+					'boards',
+					boardAndPiecesSvgSetting.board.tileTextureFilename,
+				])
+			]
+		];
+
+		//TODO this is blocking the svg loading, need to await them at the same time
+		//and then do the proper processing afterwords, depending on if the file was
+		//a texture or an svg
+		const textureRequests: Promise<[string, Blob]>[] = textureRequestInfo.map(requestInfo => {
+			return fetch(requestInfo[1]).then(response => response.blob()).then((blob) => [requestInfo[0], blob]);
+		})
+		const textures: [string, Blob][] = await Promise.all(textureRequests);
+
+		const fileReaderResults: [string, string | ArrayBuffer | null][] = await Promise.all(textures.map((textureResult) => {
+			const rawBlob = textureResult[1];
+			const fileReader = new FileReader();
+			fileReader.readAsDataURL(rawBlob);
+			const fileReaderPromise: Promise<[string, string | ArrayBuffer | null]> = new Promise((resolve, _) => {
+				fileReader.onloadend = () => {
+					resolve([textureResult[0], fileReader.result]);
+				}
+			});
+			return fileReaderPromise;
+		}));
+
+		fileReaderResults.forEach((result: [string, string | ArrayBuffer | null]) => {
+			if (typeof result[1] !== "string") {
+				throw new Error('could not convert png file to an image tag src');
+			}
+			//for debugging
+			const imageTag = document.createElement("img");
+			imageTag.src = result[1];
+			document.body.appendChild(imageTag);
+			this.images[result[0]] = imageTag;
+		});
+			//const newImage = document.createElement("img");
+			//const reader = new FileReader();
+			//reader.readAsDataURL(tex[1])
+			//reader.onloadend = () => {
+			//	const result = reader.result;
+			//	if (typeof result !== "string") throw new Error('bad image conversion');
+			//	newImage.src = result;
+			//};
+			//document.body.append(newImage);
+
+		console.log({ textures });
+
+		const svgRequestResults: [string, SVGResult][] = await Promise.all(this.loadSvgs(boardAndPiecesSvgSetting));
 		console.log('svg request results', { svgRequestResults });
 		const svgObjects: [string, Group][] = svgRequestResults.map(filenameSvgResult => {
 			const svgName = filenameSvgResult[0];
@@ -135,6 +194,9 @@ export class GameRunner {
 
 			return [svgName, group];
 		});
+
+		//Object.assign(this.images, Object.fromEntries(textures));
+
 		console.log('svg groups', svgObjects);
 		Object.assign(this.gameAssets.pieces, Object.fromEntries(svgObjects));
 		console.log({ gameAssets: this.gameAssets, pieceKeys: Object.keys(this.gameAssets.pieces) });
@@ -160,11 +222,11 @@ export class GameRunner {
 		this.setRenderer(renderer);
 	}
 
+	private formImagePath = (pathParts: string[]) => pathParts.join('/');
 
-	public prepareSvgs(boardAndPiecesSvgSetting: SvgLoadConfig): Promise<[string, SVGResult]>[] {
+	public loadSvgs(boardAndPiecesSvgSetting: SvgLoadConfig): Promise<[string, SVGResult]>[] {
 		const svgLoader = new SVGLoader();
 		//append the subpaths to the root svg lookup path
-		const formSvgPath = (pathParts: string[]) => [boardAndPiecesSvgSetting.rootDir, ...pathParts].join('/');
 
 		const piecesPaths: [string, string][] = [
 			["silver", "BlackAdvisor.svg"],
@@ -183,7 +245,8 @@ export class GameRunner {
 			["king", "BlackKing.svg"],
 		].map(pieceTuple => [
 			pieceTuple[0],
-			formSvgPath([
+			this.formImagePath([
+				boardAndPiecesSvgSetting.rootDir,
 				boardAndPiecesSvgSetting.pieceSet.setFolderName,
 				pieceTuple[1]
 			])
@@ -194,10 +257,6 @@ export class GameRunner {
 		const svgsToLoad: [string, string][] = [
 			...piecesPaths,
 			//the filename for the silver is 'advisor' for some reason
-			["tile_texture", formSvgPath([
-				'boards',
-				boardAndPiecesSvgSetting.board.tileTextureFilename,
-			])]
 		];
 
 		const svgLoadPromises = svgsToLoad.map(svgPath => new Promise<[string, SVGResult]>((resolve, _) => {
@@ -253,15 +312,161 @@ export class GameRunner {
 	private renderStep(): void {
 		const gameState = this.gameStates[this.gameStates.length - 1];
 		const scene = this.getScene();
+		const calcRenderCoordinates = this.calcRenderCoordinates(
+			gameState,
+			this.renderSettingsOrDefault()
+		);
 		//TODO skipping this, draw the pieces on the board first
 		//this.drawPieceStands(
 		//	this.getSceneGroup(SceneGroups.Stands),
 		//	gameState.viewPoint,
 		//	gameState.players,
 		//);
-		this.drawPlacedPieces(this.getSceneGroup(SceneGroups.Pieces), gameState);
+		//TODO you only need to do this once, the spaces can't movee
+		this.drawBoard(
+			this.getSceneGroup(SceneGroups.Board),
+			calcRenderCoordinates.spaceCenterPoints,
+			calcRenderCoordinates.boardWidth,
+			calcRenderCoordinates.boardHeight,
+		);
+		this.drawPlacedPieces(
+			this.getSceneGroup(SceneGroups.Pieces),
+			gameState,
+			calcRenderCoordinates.spaceCenterPoints
+		);
 
-		this.getRenderer().render(scene, this.getCamera());
+		const camera = this.getCamera();
+		//const {clientWidth, clientHeight} = this.getCanvas();
+		const {width, height} = this.getCanvas();
+		if (camera instanceof OrthographicCamera) {
+			camera.left = -width / 2;
+			camera.right = width / 2;
+			camera.top = height / 2;
+			camera.bottom = -height / 2;
+			camera.updateProjectionMatrix();
+		}
+
+		this.getRenderer().render(scene, camera);
+	}
+
+	private drawBoard(
+		boardGroup: Group,
+		spaceCenterPoints: Vector3[][],
+		boardWidth: number,
+		boardHeight: number
+	): void {
+		const boardGeometry = new PlaneGeometry(boardWidth, boardHeight);
+		const boardTexture = this.images["tile_texture"];
+		if (boardTexture === undefined) {
+			throw new Error(`draw board, no space texture`);
+		}
+		const boardMaterial = new MeshBasicMaterial({
+			map: new Texture(boardTexture)
+		});
+
+		boardGroup.remove(...boardGroup.children);
+		boardGroup.add(new Mesh(boardGeometry, boardMaterial));
+	}
+
+	/**
+	* TODO timers
+	**/
+	private calcRenderCoordinates(gameState: Game, renderSettings: RenderSettings): {
+		spaceCenterPoints: Vector3[][],
+		whitePiecesStandBasePoint: Vector3,
+		blackPiecesStandBasePoint: Vector3,
+		boardWidth: number;
+		boardHeight: number;
+	} {
+		const {files, ranks} = gameState.board;
+		const {boardSpaceWidth, boardSpaceHeight} = renderSettings;
+
+		const boardWidth = boardSpaceWidth * files;
+		const boardHeight = boardSpaceHeight * ranks;
+		const [boardTopRightCornerX, boardTopRightCornerY] = [boardWidth, boardHeight].map(dim => dim / 2);
+
+		const spaceStartPointX = boardTopRightCornerX - boardWidth / 2;
+		const spaceStartPointY = boardTopRightCornerY - boardHeight / 2;
+
+		const spaceCenterPoints: Vector3[][] = this.calcSpaceCoordinates(
+			ranks,
+			files,
+			spaceStartPointX,
+			spaceStartPointY,
+			boardSpaceWidth,
+			boardSpaceHeight
+		);
+
+		const standGap: number = 4;
+
+		const { whitePiecesStandBasePoint, blackPiecesStandBasePoint } = this.getStandCenterPoints(
+			gameState,
+			boardWidth,
+			boardHeight,
+			boardSpaceWidth,
+			boardSpaceHeight,
+			standGap
+		);
+		
+
+		return {
+			boardWidth,
+			boardHeight,
+			spaceCenterPoints,
+			whitePiecesStandBasePoint,
+			blackPiecesStandBasePoint,
+		}
+	}
+
+	private getStandCenterPoints(
+		game: Game,
+		boardWidth: number,
+		boardHeight: number,
+		boardSpaceWidth: number,
+		boardSpaceHeight: number,
+		standGap: number,
+	): { whitePiecesStandBasePoint: Vector3, blackPiecesStandBasePoint: Vector3 } {
+		//board center point to center point of piece holder
+		const standDistFromBoardCenterPointX = boardWidth / 2 + standGap + 2 * boardSpaceWidth;
+		const standDistFromBoardCenterPointY = -boardHeight / 2 + 2 * boardSpaceHeight;
+		const bottomRightStand = new Vector3(
+			standDistFromBoardCenterPointX,
+			standDistFromBoardCenterPointY,
+		);
+
+		//invert the coordinates to get the position of the other player's hand
+		const topLeftStand = bottomRightStand.clone();
+		topLeftStand.x *= -1;
+		topLeftStand.y *= -1;
+
+		return {
+			whitePiecesStandBasePoint: game.viewPoint === "white" ? bottomRightStand : topLeftStand,
+			blackPiecesStandBasePoint: game.viewPoint === "black" ? bottomRightStand : topLeftStand,
+		}
+	}
+
+	private calcSpaceCoordinates(
+		ranks: number,
+		files: number,
+		spaceStartPointX: number,
+		spaceStartPointY: number,
+		boardSpaceWidth: number,
+		boardSpaceHeight: number,
+	): Vector3[][] {
+		const spaceCenterPoints: Vector3[][] = [];
+		for(let rankIndex = 0; rankIndex < ranks; rankIndex++) {
+			const filePoints: Vector3[] = [];
+			for(let fileIndex = 0; fileIndex < files; fileIndex++) {
+				filePoints.push(new Vector3(
+					spaceStartPointX - boardSpaceWidth * fileIndex,
+					spaceStartPointY - boardSpaceHeight * rankIndex,
+					0
+				));
+			}
+			spaceCenterPoints.push(filePoints);
+		}
+
+		return spaceCenterPoints;
 	}
 
 	private drawPieceStands(standsGroup: Group, viewpoint: Turn, players: Player[]): void {
@@ -314,7 +519,11 @@ export class GameRunner {
 		});
 	}
 
-	private drawPlacedPieces(piecesGroup: Group, gameState: Game): void {
+	private drawPlacedPieces(
+		piecesGroup: Group, 
+		gameState: Game,
+		spaceCenterPointLookup: Vector3[][]
+	): void {
 		//const placedPieces = gameState.players
 		//	.flatMap(player => player.pieces)
 		//	.filter(piece => isPlaced(piece));
@@ -342,5 +551,13 @@ export class GameRunner {
 
 		piecesGroup.remove(...piecesGroup.children);
 		piecesGroup.add(...pieceGraphicsObjects);
+		placedPiecesPerPlayer.forEach((player) => {
+			player.pieces.forEach((drawPiece: DrawPiece) => {
+				//@ts-ignore TODO how do I tell the type system that these are placed pieces?
+				//I filtered them using isPlaced
+				const worldCoordinates = spaceCenterPointLookup[drawPiece.rank - 1][drawPiece.file - 1];
+				drawPiece.graphicsObject.position.copy(worldCoordinates);
+			});
+		});
 	}
 }
