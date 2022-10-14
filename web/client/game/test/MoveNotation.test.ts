34d27cfcc04125d2d30e54ad852c486afcd17e8a
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-10-15 08:19:27 +0900

    updated auto tests to reflect that the move data structure hold's its original string input from the server

--- web/client/game/test/MoveNotation.test.ts
+++ web/client/game/test/MoveNotation.test.ts
@@ -3,9 +3,30 @@ import { parseMove } from "../src/Notation/MoveNotation";
 describe("MoveNotation.ts - move parsing", () => {
 	describe("parseMove", () => {
 		const cases = [
-			{ input: "31p 41p", answer:{ start: { rank: 4, file: 2 }, end: { rank: 5, file: 2 } } },
-			{ input: "55K 66K", answer:{ start: { rank: 6, file: 6 }, end: { rank: 7, file: 7 } } },
-			{ input: "00g 01g", answer:{ start: { rank: 1, file: 1 }, end: { rank: 1, file: 2 } } },
+			{
+				input: "31p 41p",
+				answer:{
+					start: { rank: 4, file: 2 },
+					end: { rank: 5, file: 2 },
+					originalString: "31p 41p"
+				}
+			},
+			{
+				input: "55K 66K",
+				answer:{
+					start: { rank: 6, file: 6 },
+					end: { rank: 7, file: 7 },
+					originalString: "55K 66K"
+				}
+			},
+			{
+				input: "00g 01g",
+				answer:{
+					start: { rank: 1, file: 1 },
+					end: { rank: 1, file: 2 },
+					originalString:  "00g 01g",
+				}
+			},
 		];
 		test.each(cases)("%p", (testCase) => {
 			expect(parseMove(testCase.input)).toStrictEqual(testCase.answer);
