f785b9ca6cf42281523dcbf7d67526a79fba56e3
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-11-12 11:52:38 +0900

    the login script allows you to specify which container you want to log into as a command line argument

--- web/server/login_web_container.sh
+++ web/server/login_web_container.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-sudo docker exec -it server-web-1 /bin/bash
+containerArg=$1
+if [ "$containerArg" == "web" ]; then
+	targetContainer="server-web-1"
+fi
+if [ "$containerArg" == "redis" ]; then
+	targetContainer="redis-cache"
+fi
+if [ "$containerArg" == "db" ]; then
+	targetContainer="database"
+fi
+
+if [ -n "$targetContainer" ]; then
+	sudo docker exec -it $targetContainer /bin/bash
+else
+	echo "must specify a target container, valid options are: 'web' 'redis' 'db'"
+fi
