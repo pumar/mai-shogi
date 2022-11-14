510863c725dffddbf7c42c673d9d4ff475164048
Caio Pumar <caiopumar@gmail.com>
2022-11-14 17:48:59 +0900

    Initialized a rust workspace with separated tree pruning and eval libraries

---
+++ game/engine/target/debug/deps/engine_core-5a689d487b918c2e.d
@@ -0,0 +1,5 @@
+/home/cpumar/mai-shogi/mai-shogi/game/engine/target/debug/deps/engine_core-5a689d487b918c2e: engine_core/src/main.rs
+
+/home/cpumar/mai-shogi/mai-shogi/game/engine/target/debug/deps/engine_core-5a689d487b918c2e.d: engine_core/src/main.rs
+
+engine_core/src/main.rs:
---
+++ game/engine/target/debug/engine_core.d
@@ -0,0 +1,1 @@
+/home/cpumar/mai-shogi/mai-shogi/game/engine/target/debug/engine_core: /home/cpumar/mai-shogi/mai-shogi/game/engine/engine_core/src/main.rs
