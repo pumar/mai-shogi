17388e81410b406aaa84bb41232d71fea4079f8d
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-11-18 22:14:02 +0900

    bake svg script commented out code clean up

--- web/client/game/src/Game/tools/BakeSvgs.ts
+++ web/client/game/src/Game/tools/BakeSvgs.ts
@@ -1,18 +1,16 @@
 import { DoubleSide, Group, Mesh, MeshBasicMaterial, ShapeGeometry } from "three";
 import { SVGLoader, SVGResult, SVGResultPaths } from "three/examples/jsm/loaders/SVGLoader.js";
 import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils.js";
-//import { measureTime } from "../../utils/Performance";
 import * as Path from 'path';
 import * as FS from 'node:fs/promises';
 
 import { JSDOM } from "jsdom";
 
+//this is necessary so that threejs code can find the DOMParser object
+//which is needed to parse the svgs and turn them into threejs objects
 global.DOMParser = new JSDOM().window.DOMParser;
 
 function loadSvgs(paths: string[]): Promise<[string, string | undefined]>[] {
-	//const svgLoader = new SVGLoader();
-	console.log('pieces paths:', paths);
-
 	const svgFilePromises: Promise<[string, string | undefined]>[] = paths.map<Promise<[string, string | undefined]>>(svgPath => {
 		return new Promise((res, rej) => {
 			FS.readFile(svgPath, { encoding: 'utf-8' })
@@ -24,30 +22,6 @@ function loadSvgs(paths: string[]): Promise<[string, string | undefined]>[] {
 		});
 	});
 	return svgFilePromises;
-	//const svgFiles: [string, string | undefined][] = await Promise.all(svgFilePromises)
-	//const svgResults: [string, SVGResult][] = svgFiles
-	//	.filter(x => x[1] !== undefined)
-	//	.map(fileTuple => [fileTuple[0], svgLoader.parse(fileTuple[1] as string)]);
-	////console.log({ svgFiles });
-
-	//return svgResults;
-
-	//const svgs: [string, SVGElement[]] = await paths.map(svgPath => {
-	//	const prms = new Promise((resolve, _) => {
-	//		const fileData = FS.readFile(svgPath, undefined, (err, res) => {
-	//		});
-	//	})
-	//})
-	//const svgLoadPromises = paths.map(svgPath => new Promise<SVGResult>((resolve, _) => {
-	//	svgLoader.load(
-	//		svgPath[1],
-	//		(data) => {
-	//			//resolve(data)
-	//		}
-	//	);
-	//})) as Promise<SVGResult>[];
-
-	//return svgLoadPromises;
 }
 
 /**
@@ -55,7 +29,7 @@ function loadSvgs(paths: string[]): Promise<[string, string | undefined]>[] {
 * threesj objects takes like 3 seconds per game load
 * the idea is to make the meshes, and then save them to a file and just
 * load those files into the browser
-* so that they can be cast, and we don't have a slow loop running in
+* so that they can be cached, and we don't have a slow loop running in
 * the browser
 **/
 function prepareSvgGraphicsObjects(
@@ -85,8 +59,6 @@ function prepareSvgGraphicsObjects(
 			const shape = shapes[ j ];
 			const geometry = new ShapeGeometry( shape );
 			geometries.push(geometry);
-			//const mesh = new Mesh( geometry, material );
-			//group.add( mesh );
 		}
 		//merge the geometries into one geometry, and then
 		//make one mesh instead of several hundred for HUGE
@@ -96,18 +68,9 @@ function prepareSvgGraphicsObjects(
 			geometries
 		);
 		const mergedMesh = new Mesh(mergedGeometry, material);
-		//const json = mergedMesh.toJSON();
-		////jsonResults.push(json);
-		//jsonResults = json;
-		//if (i > 0) {
-		//	console.warn('I thought this loop only ran once');
-		//}
 		group.add(mergedMesh);
 	}
 	jsonResults = JSON.stringify(group.toJSON());
-	//if (jsonResults === "") {
-	//	console.error('prepareGraphicsObjects, empty json');
-	//}
 	return jsonResults;
 }
 
@@ -122,7 +85,6 @@ async function main(args: string[]): Promise<void> {
 	const scriptDirectory = __dirname;
 	const scriptPath = args[1];
 	const svgBasePath = args[2];
-	//const targetFiles = getTargetSvgs(args);
 	const svgPath = Path.resolve(
 		scriptDirectory,
 		'shogi-pieces',
@@ -131,7 +93,6 @@ async function main(args: string[]): Promise<void> {
 	console.log({ scriptDirectory, scriptPath, svgBasePath, svgPath });
 	const svgFileNames: string[] = await FS.readdir(svgPath);
 	const svgFilePaths = svgFileNames.map(fn => Path.resolve(svgPath, fn));
-	//processSvgs(svgBasePath);
 	const svgRequestResults: [string, string | undefined][] = await Promise.all(loadSvgs(svgFilePaths)).catch(e => {
 		console.error(`svg loading promise error:`, e)
 		return [];
@@ -140,21 +101,12 @@ async function main(args: string[]): Promise<void> {
 
 	const svgLoaderParseResults: [string, SVGResult][] = doSvgLoaderParsing(svgRequestResultsSuccess);
 
-	//const svgFiles: [string, string | undefined][] = await Promise.all(svgFilePromises)
-	//const svgResults: [string, SVGResult][] = svgFiles
-	//	.filter(x => x[1] !== undefined)
-	//	.map(fileTuple => [fileTuple[0], svgLoader.parse(fileTuple[1] as string)]);
-	////console.log({ svgFiles });
-	//console.log(svgRequestResults);
-
 	const jsonResults: [string, string][] = svgLoaderParseResults.map(reqResult =>
 		[reqResult[0], prepareSvgGraphicsObjects(reqResult[1].paths)]);
 	console.log(jsonResults);
 	jsonResults.forEach(async (res) => {
 		const saveJsonPath = res[0].replace('.svg', '.json');
 		console.log({ saveJsonPath });
-		//const fh = await FS.open(saveJsonPath);
-		//await fh.appendFile(res[1]);
 		await FS.appendFile(saveJsonPath, res[1]);
 	});
 }
